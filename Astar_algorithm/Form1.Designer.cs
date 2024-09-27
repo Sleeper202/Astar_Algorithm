6ba0197570da4ab832b7c3d191ed7f4a27d81b0c
Sleeper <manyinhong2022004@gmail.com>
2024-09-27 23:43:13 +0700

    Add files via upload

---
+++ Astar_algorithm/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Astar_algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            btnFindPath = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            lblResult = new Label();
+            cbStart = new ComboBox();
+            cbEnd = new ComboBox();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(650, 650);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // btnFindPath
+            // 
+            btnFindPath.Location = new Point(709, 382);
+            btnFindPath.Name = "btnFindPath";
+            btnFindPath.Size = new Size(94, 29);
+            btnFindPath.TabIndex = 1;
+            btnFindPath.Text = "Tìm";
+            btnFindPath.UseVisualStyleBackColor = true;
+            btnFindPath.Click += btnFindPath_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(698, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Bắt đầu:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(700, 171);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Kết thúc:";
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(691, 288);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(60, 20);
+            lblResult.TabIndex = 6;
+            lblResult.Text = "Kết quả";
+            // 
+            // cbStart
+            // 
+            cbStart.FormattingEnabled = true;
+            cbStart.Items.AddRange(new object[] { "A", "B", "C", "D", "E", "G", "H", "I" });
+            cbStart.Location = new Point(691, 105);
+            cbStart.Name = "cbStart";
+            cbStart.Size = new Size(151, 28);
+            cbStart.TabIndex = 7;
+            // 
+            // cbEnd
+            // 
+            cbEnd.FormattingEnabled = true;
+            cbEnd.Items.AddRange(new object[] { "A", "B", "C", "D", "E", "G", "H", "I" });
+            cbEnd.Location = new Point(691, 194);
+            cbEnd.Name = "cbEnd";
+            cbEnd.Size = new Size(151, 28);
+            cbEnd.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(944, 674);
+            Controls.Add(cbEnd);
+            Controls.Add(cbStart);
+            Controls.Add(lblResult);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnFindPath);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnFindPath;
+        private Label label1;
+        private Label label2;
+        private Label lblResult;
+        private ComboBox cbStart;
+        private ComboBox cbEnd;
+    }
+}
---
+++ Astar_algorithm/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System.Xml.Linq;
+
+namespace Astar_algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeNodes();
+        }
+        class Node
+        {
+            public string Name;
+            public List<Edge> Edges = new List<Edge>();
+            public int x;
+            public int y;
+            public float G; // Chi phí từ điểm bắt đầu
+            public float H; // Heuristic (ước lượng chi phí đến điểm đích)
+            public Node Parent; // Đỉnh cha
+
+            public float F => G + H; // Tổng chi phí
+        }
+
+        class Edge
+        {
+            public Node Destination;
+            public float Cost;
+
+            public Edge(Node destination, float cost)
+            {
+                Destination = destination;
+                Cost = cost;
+            }
+        }
+        private List<Node> nodes;
+        private void InitializeNodes()
+        {
+            nodes = new List<Node>();
+
+            Node node0 = new Node { Name = "A", x = 200, y = 50 };
+            Node node1 = new Node { Name = "B", x = 50, y = 150 };
+            Node node2 = new Node { Name = "C", x = 150, y = 150 };
+            Node node3 = new Node { Name = "D", x = 250, y = 150 };
+            Node node4 = new Node { Name = "E", x = 350, y = 150 };
+            Node node5 = new Node { Name = "G", x = 200, y = 250 };
+            Node node6 = new Node { Name = "H", x = 300, y = 250 };
+            Node node7 = new Node { Name = "I", x = 250, y = 350 };
+
+            node0.Edges.Add(new Edge(node1, 1));
+            node0.Edges.Add(new Edge(node2, 1));
+            node0.Edges.Add(new Edge(node3, 1));
+            node0.Edges.Add(new Edge(node4, 1));
+            node2.Edges.Add(new Edge(node5, 1));
+            node3.Edges.Add(new Edge(node5, 1));
+            node4.Edges.Add(new Edge(node6, 1));
+            node5.Edges.Add(new Edge(node7, 1));
+            node6.Edges.Add(new Edge(node5, 1));
+
+
+            nodes.Add(node0);
+            nodes.Add(node1);
+            nodes.Add(node2);
+            nodes.Add(node3);
+            nodes.Add(node4);
+            nodes.Add(node5);
+            nodes.Add(node6);
+            nodes.Add(node7);
+        }
+        private Node GetNodeFromInput(string input)
+        {
+            // Tìm đỉnh dựa trên tên nhập vào
+            return nodes.FirstOrDefault(n => n.Name == input);
+        }
+        class AStar
+        {
+            public List<Node> FindPath(Node start, Node goal)
+            {
+                var openSet = new List<Node> { start };
+                var closedSet = new HashSet<Node>();
+
+                while (openSet.Count > 0)
+                {
+                    // Lấy nút có giá trị F nhỏ nhất
+                    Node current = openSet[0];
+                    foreach (var node in openSet)
+                    {
+                        if (node.F < current.F)
+                        {
+                            current = node;
+                        }
+                    }
+
+                    // Nếu đã đến đích
+                    if (current == goal)
+                    {
+                        return RetracePath(current);
+                    }
+
+                    openSet.Remove(current);
+                    closedSet.Add(current);
+
+                    // Duyệt qua các cạnh
+                    foreach (var edge in current.Edges)
+                    {
+                        if (closedSet.Contains(edge.Destination)) continue;
+
+                        float tentativeG = current.G + edge.Cost;
+
+                        if (!openSet.Contains(edge.Destination))
+                        {
+                            openSet.Add(edge.Destination);
+                        }
+                        else if (tentativeG >= edge.Destination.G)
+                        {
+                            continue;
+                        }
+
+                        // Cập nhật các giá trị G, H, và Parent
+                        edge.Destination.G = tentativeG;
+                        edge.Destination.H = Heuristic(edge.Destination, goal);
+                        edge.Destination.Parent = current;
+                    }
+                }
+
+                return null; // Không tìm thấy đường
+            }
+
+            private List<Node> RetracePath(Node endNode)
+            {
+                var path = new List<Node>();
+                Node current = endNode;
+
+                while (current != null)
+                {
+                    path.Add(current);
+                    current = current.Parent;
+                }
+                path.Reverse();
+                return path;
+            }
+
+            private float Heuristic(Node a, Node b)
+            {
+                // Sử dụng khoảng cách Euclide làm heuristic (nếu có tọa độ)
+                // return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+                return 0; // Cập nhật theo cách bạn muốn
+            }
+        }
+
+        private void btnFindPath_Click(object sender, EventArgs e)
+        {
+
+
+            // Lấy điểm bắt đầu và kết thúc từ TextBox
+            Node startNode = GetNodeFromInput(cbStart.Text);
+            Node endNode = GetNodeFromInput(cbEnd.Text);
+
+            // Gọi hàm tìm đường
+            AStar aStar = new AStar();
+            List<Node> path = aStar.FindPath(startNode, endNode);
+
+            // Hiển thị kết quả
+            if (path != null)
+            {
+                lblResult.Text = "Kết quả:\r\nĐường đi: " + string.Join(" -> ", path.Select(n => n.Name));
+            }
+            else
+            {
+                lblResult.Text = "Kết quả:\r\nKhông tìm thấy đường!";
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Vẽ các đỉnh
+            foreach (var node in nodes)
+            {
+                g.FillEllipse(Brushes.Blue, node.x, node.y, 30, 30);
+                g.DrawString(node.Name, new Font("Arial", 10), Brushes.White, node.x,node.y);
+            }
+
+            // Vẽ các cạnh
+            foreach (var node in nodes)
+            {
+                foreach (var edge in node.Edges)
+                {
+                    g.DrawLine(Pens.Black, node.x+15,node.y+15,edge.Destination.x+15,edge.Destination.y+15);
+                }
+            }
+        }
+    }
+}
